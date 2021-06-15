800b716c98ad9d1cf17a4bcd3c02afd6f247d765
abritopach <adrbrpa1988@gmail.com>
2021-06-15 23:05:11 +0100

    ⚡ Added footer

--- front/src/app/home/home.module.ts
+++ front/src/app/home/home.module.ts
@@ -6,13 +6,14 @@ import { HomePage } from './home.page';
 
 import { HomePageRoutingModule } from './home-routing.module';
 
-
+import { NtkmeButtonModule } from '@ctrl/ngx-github-buttons';
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    HomePageRoutingModule
+    HomePageRoutingModule,
+    NtkmeButtonModule
   ],
   declarations: [HomePage]
 })
--- front/src/app/home/home.page.ts
+++ front/src/app/home/home.page.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+    selector: 'app-home',
+    templateUrl: 'home.page.html',
+    styleUrls: ['home.page.scss'],
 })
 export class HomePage {
 
-  constructor() {}
+    currentYear = new Date().getFullYear();
+
+    constructor() {}
 
 }
