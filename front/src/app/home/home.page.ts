5a84bc6e4c7b2ad161e0bb985ba10c114232e3d4
abritopach <adrbrpa1988@gmail.com>
2021-06-22 22:58:01 +0100

    ⚡ Added video info model & Updated functionality

--- front/src/app/home/home.page.ts
+++ front/src/app/home/home.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormatType } from '../models/format.model';
+import { IVideoInfo } from '../models/video-infot.model';
 import { ApiService } from '../services/api/api.service';
 
 @Component({
@@ -10,16 +11,25 @@ import { ApiService } from '../services/api/api.service';
 export class HomePage {
 
     currentYear = new Date().getFullYear();
-    format: FormatType;
+    format = FormatType;
     isDownloadButtonClicked = false;
+    videoInfo: IVideoInfo = {
+        url: '',
+        format: FormatType.MP3
+    };
 
     constructor(private apiService: ApiService) {}
 
     downloadYoutubeVideo() {
-        console.log('HomePage::downloadYoutubeVideo method called');
+        console.log('HomePage::downloadYoutubeVideo method called', this.videoInfo);
         this.apiService.downloadVideo({}).subscribe(result => {
             console.log('result', result);
         })
+    }
+
+    videoFormatChanged(event: any) {
+        console.log('HomePage::videoFormatChanged method called', event.detail.value);
+        this.videoInfo.format = event.detail.value;
     }
 
 }
---
+++ front/src/app/models/video-infot.model.ts
@@ -0,0 +1,15 @@
+import { FormatType } from "./format.model";
+
+/**
+ * Description [Interface to define video info data.]
+ *
+ * @author abrito
+ * @version 0.0.1
+ *
+ * @interface
+*/
+
+export interface IVideoInfo {
+    url: string;
+    format: FormatType
+}
