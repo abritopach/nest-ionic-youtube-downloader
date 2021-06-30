cd6055c337c4130ddadd5a282d17bedc4029f449
abritopach <adrbrpa1988@gmail.com>
2021-06-30 23:09:36 +0100

    🚀 Refactor code

--- front/src/app/home/home.page.ts
+++ front/src/app/home/home.page.ts
@@ -36,11 +36,11 @@ export class HomePage {
         const blob = new Blob([new Uint8Array(downloadVideoData['data'])], { type: ACCEPT_MIME_TYPES.get(this.videoInfo.format)});
         if (this.videoInfo.format === FormatType.MP4) {
             saveAs(blob, `${checkVideoData.title}.${this.videoInfo.format.toLocaleLowerCase()}`);
+            this.hideLoading();
         }
         else {
             this.convertToMP3(blob, checkVideoData);
         }
-        this.hideLoading();
     }
 
     videoFormatChanged(event: any) {
@@ -67,9 +67,7 @@ export class HomePage {
     }
 
     isValidYouTubeVideoUrl(url: string) {
-        console.log('isValidYouTubeVideoUrl', url);
         const youtubeRegExp = /^(?:https?:\/\/)?(?:m\.|www\.)?(?:youtu\.be\/|youtube\.com\/(?:embed\/|v\/|watch\?v=|watch\?.+&v=))((\w|-){11})(?:\S+)?$/;
-        console.log(url.match(youtubeRegExp));
         if (url.match(youtubeRegExp)) {
             return true;
         }
@@ -79,37 +77,30 @@ export class HomePage {
     convertToMP3(blob: Blob, checkVideoData: IVideoCheckResponse) {
         const audioContext = new AudioContext();
         const fileReader = new FileReader();
-
-        // Set up file reader on loaded end event
+        // Set up file reader on loaded end event.
         fileReader.onloadend = () => {
             const arrayBuffer = fileReader.result as ArrayBuffer;
 
-            // Convert array buffer into audio buffer
+            // Convert array buffer into audio buffer.
             audioContext.decodeAudioData(arrayBuffer, (audioBuffer) => {
-
-                // Do something with audioBuffer
                 console.log(audioBuffer)
                 const mp3Blob = this.audioBufferToWav(audioBuffer);
                 saveAs(mp3Blob, `${checkVideoData.title}.${this.videoInfo.format.toLocaleLowerCase()}`);
+                this.hideLoading();
 
             })
         }
-
         //Load blob
         fileReader.readAsArrayBuffer(blob)
     }
 
-    /*
-    audioBufferToWav(aBuffer) {
-        let numOfChan = aBuffer.numberOfChannels,
+    audioBufferToWav(aBuffer: AudioBuffer) {
+        const numOfChan = aBuffer.numberOfChannels,
             btwLength = aBuffer.length * numOfChan * 2 + 44,
             btwArrBuff = new ArrayBuffer(btwLength),
             btwView = new DataView(btwArrBuff),
-            btwChnls = [],
-            btwIndex,
-            btwSample,
-            btwOffset = 0,
-            btwPos = 0;
+            btwChnls = [];
+        let btwSample: number, btwPos = 0, btwOffset = 0;
         setUint32(0x46464952); // "RIFF"
         setUint32(btwLength - 8); // file length - 8
         setUint32(0x45564157); // "WAVE"
@@ -124,93 +115,11 @@ export class HomePage {
         setUint32(0x61746164); // "data" - chunk
         setUint32(btwLength - btwPos - 4); // chunk length
 
-        for (btwIndex = 0; btwIndex < aBuffer.numberOfChannels; btwIndex++)
+        for (let btwIndex = 0; btwIndex < aBuffer.numberOfChannels; btwIndex++)
             btwChnls.push(aBuffer.getChannelData(btwIndex));
 
         while (btwPos < btwLength) {
-            for (btwIndex = 0; btwIndex < numOfChan; btwIndex++) {
-                // interleave btwChnls
-                btwSample = Math.max(-1, Math.min(1, btwChnls[btwIndex][btwOffset])); // clamp
-                btwSample = (0.5 + btwSample < 0 ? btwSample * 32768 : btwSample * 32767) | 0; // scale to 16-bit signed int
-                btwView.setInt16(btwPos, btwSample, true); // write 16-bit sample
-                btwPos += 2;
-            }
-            btwOffset++; // next source sample
-        }
-
-        let wavHdr = lamejs.WavHeader.readHeader(new DataView(btwArrBuff));
-        let wavSamples = new Int16Array(btwArrBuff, wavHdr.dataOffset, wavHdr.dataLen / 2);
-
-        this.wavToMp3(wavHdr.channels, wavHdr.sampleRate, wavSamples);
-
-        function setUint16(data) {
-            btwView.setUint16(btwPos, data, true);
-            btwPos += 2;
-        }
-
-        function setUint32(data) {
-            btwView.setUint32(btwPos, data, true);
-            btwPos += 4;
-        }
-    }
-
-    wavToMp3(channels, sampleRate, samples) {
-        var buffer = [];
-        var mp3enc = new lamejs.Mp3Encoder(channels, sampleRate, 128);
-        console.log('mp3enc', mp3enc);
-        var remaining = samples.length;
-        var samplesPerFrame = 1152;
-        for (var i = 0; remaining >= samplesPerFrame; i += samplesPerFrame) {
-            var mono = samples.subarray(i, i + samplesPerFrame);
-            var mp3buf = mp3enc.encodeBuffer(mono);
-            if (mp3buf.length > 0) {
-                buffer.push(new Int8Array(mp3buf));
-            }
-            remaining -= samplesPerFrame;
-        }
-        var d = mp3enc.flush();
-        if(d.length > 0){
-            buffer.push(new Int8Array(d));
-        }
-
-        var mp3Blob = new Blob(buffer, {type: 'audio/mp3'});
-        var bUrl = window.URL.createObjectURL(mp3Blob);
-
-        // send the download link to the console
-        console.log('mp3 download:', bUrl);
-
-    }
-    */
-
-    audioBufferToWav(aBuffer) {
-        let numOfChan = aBuffer.numberOfChannels,
-            btwLength = aBuffer.length * numOfChan * 2 + 44,
-            btwArrBuff = new ArrayBuffer(btwLength),
-            btwView = new DataView(btwArrBuff),
-            btwChnls = [],
-            btwIndex,
-            btwSample,
-            btwOffset = 0,
-            btwPos = 0;
-        setUint32(0x46464952); // "RIFF"
-        setUint32(btwLength - 8); // file length - 8
-        setUint32(0x45564157); // "WAVE"
-        setUint32(0x20746d66); // "fmt " chunk
-        setUint32(16); // length = 16
-        setUint16(1); // PCM (uncompressed)
-        setUint16(numOfChan);
-        setUint32(aBuffer.sampleRate);
-        setUint32(aBuffer.sampleRate * 2 * numOfChan); // avg. bytes/sec
-        setUint16(numOfChan * 2); // block-align
-        setUint16(16); // 16-bit
-        setUint32(0x61746164); // "data" - chunk
-        setUint32(btwLength - btwPos - 4); // chunk length
-
-        for (btwIndex = 0; btwIndex < aBuffer.numberOfChannels; btwIndex++)
-            btwChnls.push(aBuffer.getChannelData(btwIndex));
-
-        while (btwPos < btwLength) {
-            for (btwIndex = 0; btwIndex < numOfChan; btwIndex++) {
+            for (let btwIndex = 0; btwIndex < numOfChan; btwIndex++) {
                 // interleave btwChnls
                 btwSample = Math.max(-1, Math.min(1, btwChnls[btwIndex][btwOffset])); // clamp
                 btwSample = (0.5 + btwSample < 0 ? btwSample * 32768 : btwSample * 32767) | 0; // scale to 16-bit signed int
@@ -223,25 +132,24 @@ export class HomePage {
         let wavHdr = lamejs.WavHeader.readHeader(new DataView(btwArrBuff));
 
         //Stereo
-        let data = new Int16Array(btwArrBuff, wavHdr.dataOffset, wavHdr.dataLen / 2);
-        let leftData = [];
-        let rightData = [];
+        const data = new Int16Array(btwArrBuff, wavHdr.dataOffset, wavHdr.dataLen / 2);
+        const leftData = [];
+        const rightData = [];
         for (let i = 0; i < data.length; i += 2) {
             leftData.push(data[i]);
             rightData.push(data[i + 1]);
         }
-        var left = new Int16Array(leftData);
-        var right = new Int16Array(rightData);
+        const left = new Int16Array(leftData);
+        const right = new Int16Array(rightData);
 
 
-        if (this.videoInfo.format === 'MP3')
-        {
+        if (this.videoInfo.format === 'MP3') {
             //STEREO
             if (wavHdr.channels===2)
-                return this.wavToMp3(wavHdr.channels, wavHdr.sampleRate,  left,right);
+                return this.wavToMp3(wavHdr.channels, wavHdr.sampleRate, left, right);
             //MONO
             else if (wavHdr.channels===1)
-                return this.wavToMp3(wavHdr.channels, wavHdr.sampleRate,  data);
+                return this.wavToMp3(wavHdr.channels, wavHdr.sampleRate, data);
         }
         else
             return new Blob([btwArrBuff], {type: "audio/wav"});
@@ -257,33 +165,34 @@ export class HomePage {
         }
     }
 
-    wavToMp3(channels, sampleRate, left, right = null) {
-        var buffer = [];
-        var mp3enc = new lamejs.Mp3Encoder(channels, sampleRate, 128);
-        var remaining = left.length;
-        var samplesPerFrame = 1152;
+    wavToMp3(channels: number, sampleRate: number, left: Int16Array, right: Int16Array = null) {
+        const buffer = [];
+        const mp3enc = new lamejs.Mp3Encoder(channels, sampleRate, 128);
+        let remaining = left.length;
+        const samplesPerFrame = 1152;
+        let mp3buf;
 
-        for (var i = 0; remaining >= samplesPerFrame; i += samplesPerFrame) {
+        for (let i = 0; remaining >= samplesPerFrame; i += samplesPerFrame) {
             if (!right)
             {
-                var mono = left.subarray(i, i + samplesPerFrame);
-                var mp3buf = mp3enc.encodeBuffer(mono);
+                const mono = left.subarray(i, i + samplesPerFrame);
+                mp3buf = mp3enc.encodeBuffer(mono);
             }
             else {
-                var leftChunk = left.subarray(i, i + samplesPerFrame);
-                var rightChunk = right.subarray(i, i + samplesPerFrame);
-                var mp3buf = mp3enc.encodeBuffer(leftChunk,rightChunk);
+                const leftChunk = left.subarray(i, i + samplesPerFrame);
+                const rightChunk = right.subarray(i, i + samplesPerFrame);
+                mp3buf = mp3enc.encodeBuffer(leftChunk,rightChunk);
             }
                 if (mp3buf.length > 0) {
                         buffer.push(mp3buf);//new Int8Array(mp3buf));
                 }
                 remaining -= samplesPerFrame;
         }
-        var d = mp3enc.flush();
+        const d = mp3enc.flush();
         if(d.length > 0){
                 buffer.push(new Int8Array(d));
         }
-        var mp3Blob = new Blob(buffer, {type: 'audio/mp3'});
+        const mp3Blob = new Blob(buffer, {type: 'audio/mp3'});
         //var bUrl = window.URL.createObjectURL(mp3Blob);
         // send the download link to the console
         //console.log('mp3 download:', bUrl);
