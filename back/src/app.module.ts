778eef39de1df34e1d4e7f7e4592956f245118fa
abritopach <adrbrpa1988@gmail.com>
2021-07-14 18:19:19 +0100

    Closes #5: ⚡ Add health check endpoint

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -3,10 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { YoutubeVideoDownloaderController } from './controllers/youtube-video-downloader/youtube-video-downloader.controller';
 import { YoutubeVideoDownloaderService } from './services/youtube-video-downloader/youtube-video-downloader.service';
+import { HealthCheckController } from './controllers/health-check/health-check.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController, YoutubeVideoDownloaderController],
+  controllers: [AppController, YoutubeVideoDownloaderController, HealthCheckController],
   providers: [AppService, YoutubeVideoDownloaderService],
 })
 export class AppModule {}
