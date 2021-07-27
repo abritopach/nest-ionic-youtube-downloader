21de7eaa161e2acea6661d4e067e79dca31d8f85
abritopach <adrbrpa1988@gmail.com>
2021-07-27 20:21:21 +0100

    ⚡ Updated cors in main.ts

--- back/src/main.ts
+++ back/src/main.ts
@@ -2,13 +2,15 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
+  /*
   app.enableCors(
     {
     origin: '*',
     methods: 'GET, PUT, POST, DELETE',
     allowedHeaders: 'Content-Type, Authorization',
   });
+  */
   await app.listen(3000);
 }
 bootstrap();
