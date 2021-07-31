02a407564192468a01c98153da41024ef644a280
abritopach <adrbrpa1988@gmail.com>
2021-07-31 22:35:57 +0100

    Closes #6: 🚀 Deploying the back-end project in Heroku

--- front/src/environments/environment.prod.ts
+++ front/src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  API_URL_BASE: 'https://youtube-video-downloader-api.vercel.app',
+  // API_URL_BASE: 'https://youtube-video-downloader-api.vercel.app',
+  API_URL_BASE: 'https://youtube-video-downloader-api.herokuapp.com'
 };
