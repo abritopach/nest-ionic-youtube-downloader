97b1fb22e5f1e59c82776ef020e9493008099ca2
abritopach <adrbrpa1988@gmail.com>
2021-09-02 22:43:39 +0100

    🎨 Updated youtube downloader info componentstyles

--- front/src/app/components/youtube-downloader-info/youtube-downloader-info/youtube-downloader-info.component.ts
+++ front/src/app/components/youtube-downloader-info/youtube-downloader-info/youtube-downloader-info.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ModalController } from '@ionic/angular';
 
 @Component({
   selector: 'app-youtube-downloader-info',
@@ -7,8 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class YoutubeDownloaderInfoComponent implements OnInit {
 
-  constructor() { }
+  constructor(private modalController: ModalController) { }
 
   ngOnInit() {}
+
+  dismissModal() {
+    this.modalController.dismiss();
+  }
 
 }
